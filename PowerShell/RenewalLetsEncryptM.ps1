f3821d0d32abaad584cc4fd871b799a4d60561d7
Alexey Bobrov <61884077+alex11087@users.noreply.github.com>
2022-10-26 19:01:42 +0200

    Create RenewalLetsEncryptM.ps1

---
+++ PowerShell/RenewalLetsEncryptM.ps1
@@ -0,0 +1,156 @@
+#######################################################################################
+# Script that renews a Let's Encrypt certificate for an Azure Application Gateway
+# Pre-requirements:
+#      - Have a storage account in which the folder path has been created: 
+#        '/.well-known/acme-challenge/', to put here the Let's Encrypt DNS check files
+
+#      - Add "Path-based" rule in the Application Gateway with this configuration: 
+#           - Path: '/.well-known/acme-challenge/*'
+#           - Check the configure redirection option
+#           - Choose redirection type: permanent
+#           - Choose redirection target: External site
+#           - Target URL: <Blob public path of the previously created storage account>
+#                - Example: 'https://test.blob.core.windows.net/public'
+#      - For execution on Azure Automation: Import 'AzureRM.profile', 'AzureRM.Network' 
+#        and 'ACMESharp' modules in Azure
+#
+#      UPDATE 2019-11-27
+#      - Due to deprecation of ACMEv1, a new script is required to use ACMEv2.
+#        The module to use is called ACME-PS.
+#
+#      UPDATE 2020-09-03
+#      - Migrated to Az modules.
+#        Following modules are needed now: Az.Accounts, Az.Network, Az.Storage
+#
+#######################################################################################
+
+Param(
+    [string]$domain,
+    [string]$EmailAddress,
+    [string]$STResourceGroupName,
+    [string]$storageName,
+    [string]$AGResourceGroupName,
+    [string]$AGName,
+    [string]$AGOldCertName
+)
+
+# Ensures that no login info is saved after the runbook is done
+Disable-AzContextAutosave
+
+# Log in as the service principal from the Runbook
+$connection = Get-AutomationConnection -Name AzureRunAsConnection
+Login-AzAccount -ServicePrincipal -Tenant $connection.TenantID -ApplicationId $connection.ApplicationID -CertificateThumbprint $connection.CertificateThumbprint
+
+# Create a state object and save it to the harddrive
+$state = New-ACMEState -Path $env:TEMP
+$serviceName = 'LetsEncrypt'
+
+# Fetch the service directory and save it in the state
+Get-ACMEServiceDirectory -State $state -ServiceName $serviceName -PassThru;
+
+# Get the first anti-replay nonce
+New-ACMENonce -State $state;
+
+# Create an account key. The state will make sure it's stored.
+New-ACMEAccountKey -State $state -PassThru;
+
+# Register the account key with the acme service. The account key will automatically be read from the state
+New-ACMEAccount -State $state -EmailAddresses $EmailAddress -AcceptTOS;
+
+# Load an state object to have service directory and account keys available
+$state = Get-ACMEState -Path $env:TEMP;
+
+# It might be neccessary to acquire a new nonce, so we'll just do it for the sake of the example.
+New-ACMENonce -State $state -PassThru;
+
+# Create the identifier for the DNS name
+#$identifier = New-ACMEIdentifier $domain;
+
+
+$identifier = @(
+	New-ACMEIdentifier $domain;
+	New-ACMEIdentifier "www.$domain";
+)
+
+# Create the order object at the ACME service.
+$order = New-ACMEOrder -State $state -Identifiers $identifier;
+
+$authorizations = @(Get-ACMEAuthorization -State $state -Order $order);
+
+foreach($authz in $authorizations) {
+    $challenge = Get-ACMEChallenge -State $state -Authorization $authz -Type "http-01"
+
+    $challenge.Data
+
+    $chFilename = [System.IO.Path]::Combine($documentRoot, $challenge.Data.RelativeUrl);
+    $chDirectory = [System.IO.Path]::GetDirectoryName($chFilename);
+
+    # Ensure the challenge directory exists
+    if(-not (Test-Path $chDirectory)) {
+        New-Item -Path $chDirectory -ItemType Directory;
+    }
+
+    Set-Content -Path $chFilename -Value $challenge.Data.Content -NoNewline;
+
+    $blobName = ".well-known/acme-challenge/" + $challenge.Token
+    $storageAccount = Get-AzStorageAccount -ResourceGroupName $STResourceGroupName -Name $storageName
+    $ctx = $storageAccount.Context
+
+    Set-AzStorageBlobContent -File $chFilename -Container "public" -Context $ctx -Blob $blobName
+
+    # Signal the ACME server that the challenge is ready
+    $challenge | Complete-ACMEChallenge -State $state;
+}
+
+# Wait a little bit and update the order, until we see the states
+while($order.Status -notin ("ready","invalid")) {
+    Start-Sleep -Seconds 10;
+    $order | Update-ACMEOrder $state -PassThru;
+}
+
+# Should the order get invalid, use Get-ACMEAuthorizationError to list error details.
+if($order.Status -ieq ("invalid")) {
+    $order | Get-ACMEAuthorizationError -State $state;
+    throw "Order was invalid";
+}
+
+Import-Module 'ACME-PS';
+
+if ($null -ne $order) { # Will fetch the order
+    $order = Find-ACMEOrder -State $state -Identifiers $identifier;
+}
+
+# Wait a little bit and update the order, until we see the status 'ready' or 'invalid'
+while($order.Status -notin ("ready","invalid")) {
+    Start-Sleep -Seconds 10;
+    $order | Update-ACMEOrder $state -PassThru;
+}
+
+if($order.Status -eq "invalid") {
+    throw "Your order has been marked as invalid - certificate cannot be issued."
+}
+
+# We should have a valid order now and should be able to complete it
+# Therefore we need a certificate key
+$certKey = New-ACMECertificateKey -Path "$env:TEMP\$domain.key.xml";
+
+# Complete the order - this will issue a certificate singing request
+Complete-ACMEOrder $state -Order $order -CertificateKey $certKey;
+
+# Now we wait until the ACME service provides the certificate url
+while(-not $order.CertificateUrl) {
+    Start-Sleep -Seconds 15
+    $order | Update-Order $state -PassThru
+}
+
+# As soon as the url shows up we can create the PFX
+$password = ConvertTo-SecureString -String "Passw@rd123***" -Force -AsPlainText
+Export-ACMECertificate $state -Order $order -CertificateKey $certKey -Path "$env:TEMP\$domain.pfx" -Password $password;
+
+# Delete blob to check DNS
+Remove-AzStorageBlob -Container "public" -Context $ctx -Blob $blobName
+
+### RENEW APPLICATION GATEWAY CERTIFICATE ###
+$appgw = Get-AzApplicationGateway -ResourceGroupName $AGResourceGroupName -Name $AGName
+Set-AzApplicationGatewaySSLCertificate -Name $AGOldCertName -ApplicationGateway $appgw -CertificateFile "$env:TEMP\$domain.pfx" -Password $password
+Set-AzApplicationGateway -ApplicationGateway $appgw
